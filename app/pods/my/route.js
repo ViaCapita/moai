d8362341ad36d035a46ef7e06da3bece7b916011
Brendan OHara <brendan@oharas.org>
2015-03-25 14:17:54 -0400

    Persist sessions

--- app/pods/my/route.js
+++ app/pods/my/route.js
@@ -1,12 +1,13 @@
 import Ember from "ember";
 
 export default Ember.Route.extend({
-  beforeModel: function() {
+  model: function() {
     if(!this.session.get('user')) {
       var localSession = JSON.parse(localStorage.getItem("localSession"));
 
-      if(localSession.user) {
-        this.session.set('user', localSession.user);
+      if(localSession) {
+        this.session.set('userSession', localSession);
+        this.session.set('user', this.store.find('user', localSession.uid));
       }
       else {
         this.transitionTo('signin');          
