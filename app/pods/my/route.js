daba33dcffb62d3a06c75f6764513b914916188e
CodeOfficer <codeofficer@gmail.com>
2015-03-26 16:13:28 -0400

    fixes reload somewhat …

--- app/pods/my/route.js
+++ app/pods/my/route.js
@@ -2,39 +2,45 @@ import Ember from "ember";
 
 export default Ember.Route.extend({
   model: function(){
-    var that   = this;
+    var _this   = this;
+    var store  = this.get('store');
     var storage = JSON.parse(localStorage.getItem("firebase:session::pairsapp"));
-    // var localSession = JSON.parse(localStorage.getItem("localSession"));
     var fb      = new window.Firebase('https://pairsapp.firebaseio.com');
-    
-    if (!storage) { 
-      this.transitionTo('signin'); 
-    } else 
-    if(!that.session.get('user')) {
-      fb.authWithCustomToken(storage.token, function(error, authData) {
-        if (error) {
-          this.transitionTo('signin'); 
-        } else {
-          console.log("Authenticated successfully with token:", authData);
-          var user = that.get("store").find('user', authData.uid);
-          var us   = that.get('store').createRecord('session');
-          us.setProperties({
-            user:          user,
-            uid:           authData.uid,
-            provider:      authData.provider,
-            auth:          authData.auth,
-            expires:       authData.expires,
-            email:         authData.email,
-            resetPassword: authData.isTemporaryPassword
-          });
-          us.save().then(function () { 
-            that.session.set('user', user);
-            that.session.set('userSession', us);
-            localStorage.setItem("localSession", JSON.stringify(us));
-            that.transitionTo("chat");
-          });
-        }
+
+    if (!storage) {
+      this.transitionTo('signin');
+    } else if(!_this.session.get('user')) {
+      var promise = new Promise(function(resolve, reject) {
+        fb.authWithCustomToken(storage.token, function(error, authData) {
+          if (error) {
+            _this.transitionTo('signin');
+            reject('failed login');
+          } else {
+            console.log("Authenticated successfully with token:", authData);
+            var user = store.find('user', authData.uid);
+
+            user.then(function(user){
+              var session   = store.createRecord('session', {
+                user:          user,
+                uid:           authData.uid,
+                provider:      authData.provider,
+                auth:          authData.auth,
+                expires:       authData.expires,
+                resetPassword: authData.isTemporaryPassword
+              });
+              session.save().then(function () {
+                _this.session.set('user', user);
+                _this.session.set('userSession', session);
+                localStorage.setItem("localSession", JSON.stringify(session));
+                _this.transitionTo("chat");
+                resolve(true);
+              });
+            });
+          }
+        });
       });
+
+      return promise;
     }
   }
 });
